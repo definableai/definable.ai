80659c8d17c804d6d6e1ed9fdc5c11906202dbad
Anandesh Sharma <anandeshsharma@gmail.com>
2026-04-10 00:15:16 +0530

    Remove vectordb deprecated add(), aliases, and compat module
    Drop InMemoryVectorDB.add() and dimensions kwarg. Remove
    RedisVectorDb and MongoVectorDb backward-compat aliases.
    Delete utils/vectordb.py (re-export shim for Distance/SearchType).

--- definable/definable/vectordb/mongodb/__init__.py
+++ definable/definable/vectordb/mongodb/__init__.py
@@ -1,11 +1,7 @@
 from definable.vectordb.mongodb.mongodb import MongoDb
 from definable.vectordb.search import SearchType
 
-# Alias to avoid name collision with the main MongoDb class
-MongoVectorDb = MongoDb
-
 __all__ = [
-  "MongoVectorDb",
   "MongoDb",
   "SearchType",
 ]
